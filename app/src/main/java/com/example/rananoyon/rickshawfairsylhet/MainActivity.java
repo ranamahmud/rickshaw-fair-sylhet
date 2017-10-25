14fe6af572cb2f28d3216c16053b355fa7261d7f
Md Rana Mahmud <ranamd0@gmail.com>
2017-10-25 21:22:22 +0600

    Floating Action Bar Removed

--- app/src/main/java/com/example/rananoyon/rickshawfairsylhet/MainActivity.java
+++ app/src/main/java/com/example/rananoyon/rickshawfairsylhet/MainActivity.java
@@ -59,14 +59,14 @@ public class MainActivity extends AppCompatActivity implements Time.OnFragmentIn
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
     }
 
